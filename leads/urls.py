5999aa9aca34a01d28dd303d9657267c948f8a7a
rajuk_regalix <rajuk_regalix@localhost>
2015-02-12 16:25:09 +0530

    bulk upload feature

---
+++ leads/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import url, patterns
+import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^lead-form/$', views.lead_form),
+    url(r'^shopping-campaign-setup-lead-form/$', views.shopping_campaign_setup_lead_form),
+    url(r'^shopping-campaign-lead-form/$', views.shopping_campaign_lead_form),
+    url(r'^leads-list/$', views.leads_list),
+    url(r'^leads-report/$', views.leads_report),
+    url(r'^thankyou/$', views.thankyou),
+    url(r'^day-light-changes/$', views.day_light_changes),
+    url(r'^manage-leads/$', views.manage_leads),
+    url(r'^upload-leads/$', views.upload_leads),
+    url(r'^migrate-leads/$', views.migrate_leads),
+    url(r'^get-lead/(?P<cid>[\w -]+)$', views.get_lead),
+    url(r'^lead-summary/$', views.get_lead_summary),
+    url(r'^lead-summary/(?P<lid>[0-9]+)$', views.get_lead_summary),
+    url(r'^create-chat/$', views.create_chat_message),
+    url(r'^create-teams/$', views.create_teams),
+    url(r'^create-locations/$', views.create_locations),
+    url(r'^create-codetypes/$', views.create_codetypes),
+    url(r'^agency-form/$', views.agency_form),
+    url(r'^download-agency-csv/$', views.download_agency_csv),
+    url(r'^get-chat/$', views.get_chat_message_by_lead),
+    url(r'^agency/(?P<agency_name>[\w -]+)/(?P<pid>[\w -][^/]+)$', views.agent_bulk_upload),
+)
