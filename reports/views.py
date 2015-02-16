e9e06ddcf2dd4d83f4e21c18003b3e5f560af0b2
Basava <basavaraju@regalix-inc.com>
2015-02-16 19:16:06 +0530

    ldap issue in Report has been fixed

--- reports/views.py
+++ reports/views.py
@@ -3,8 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 import json
 from datetime import datetime
-import time
-from leads.models import Leads, Location, RegalixTeams
+from leads.models import Location
 from report_services import ReportService, DownloadLeads, TrendsReportServices
 from lib.helpers import get_quarter_date_slots, is_manager, get_user_under_manager
 from django.conf import settings
@@ -242,6 +241,8 @@ def get_download_report(request):
 def get_user_name(request):
     if request.is_ajax():
         query = request.GET.get('term', '')
+        if ' ' in query:
+            query = query.split(' ')[0]
         qs = User.objects.all()
         users = qs.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query))
         results = []
--- static/js/reports.js
+++ static/js/reports.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $("#split_location").hide();
 /* ===================== Default Report Starts Here ============= */
   // Get default report while loading template
-  callAjax({'report_type': 'default_report', 'report_timeline': ['today'], 'teams': ['all'], 'countries': ['all']})
+  callAjax({'report_type': 'default_report', 'report_timeline': ['today'], 'team': ['all'], 'countries': ['all']})
 /* ===================== Default Report Ends Here ============= */
 
 // date picker
@@ -229,7 +229,6 @@ $("#get_report").click(function(){
         dataString['location_split'] = $('#location_split').prop('unchecked');
       }
 
-     console.log(dataString, "Calling");
 
      if(window.current_ldap){
       dataString['ldap_id'] = window.current_ldap;
@@ -302,6 +301,8 @@ function showErrorMessage(message){
 }
 
 function setDefaultDropdown(){
+  $("#filter_timeline").val('')
+  $("#filter_dateRange").hide()
   $("#filter_region select").val('');
   $("#filter_team").val('all');
 }
@@ -812,7 +813,7 @@ function downloadReport(dataString){
 
 }
 
-$('.ldap').focus(function() {
+$('.ldap').on("keyup", function() {
   return $(this).autocomplete({
     source: "/reports/get-user-name",
     minLength: 2,
