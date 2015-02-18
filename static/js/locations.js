359bdb6aa84dda43dd8cf6e450705e86d170dc2b
rajuk_regalix <rajuk_regalix@localhost>
2015-02-18 20:01:09 +0530

    bundle form updates

--- static/js/locations.js
+++ static/js/locations.js
@@ -1,13 +1,4 @@
 var placeSearch, autocomplete;
-var componentForm = {
-  street_number: 'short_name',
-  route: 'long_name',
-  locality: 'long_name',
-  administrative_area_level_1: 'short_name',
-  country: 'long_name',
-  postal_code: 'short_name'
-};
-
 function initialize() {
   // Create the autocomplete object, restricting the search
   // to geographical location types.
