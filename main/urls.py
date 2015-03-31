62aeec930850d11623798494ca1c8451a6f51029
Basava <basavaraju@regalix-inc.com>
2015-03-31 12:47:05 +0530

    Report Rework for Request Time out

---
+++ main/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import patterns, url
+from main import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.home),
+    url(r'^home$', views.main_home),
+    url(r'^edit-profile-info/$', views.edit_profile_info),
+    url(r'^team/$', views.team),
+    url(r'^feedback/create/$', views.create_feedback),
+    url(r'^feedback/create/(?P<lead_id>\d+)/$', views.create_feedback),
+    url(r'^feedback/list/$', views.list_feedback),
+    url(r'^feedback/view/Goog-(?P<id>\d+)/$', views.view_feedback),
+    url(r'^create-feedback-from-lead-status$', views.create_feedback_from_lead_status),
+    url(r'^feedback/(?P<id>\d+)/comment/$', views.comment_feedback),
+    # url(r'^feedback/(?P<id>\d+)/resolve/$', views.resolve_feedback),
+    url(r'^feedback/(?P<id>\d+)/reopen/$', views.reopen_feedback),
+    url(r'^resources/$', views.resources),
+    url(r'^get-inbound-locations$', views.get_inbound_locations),
+    url(r'^get-notifications$', views.get_notifications),
+    url(r'^get-started/$', views.get_started),
+    url(r'^portal/feedback$', views.create_portal_feedback),
+    url(r'^sales-tasks/$', views.sales_tasks),
+    # url(r'^contacts/$', views.get_contacts),
+    url(r'^top-30-cms/$', views.top_30_cms),
+    url(r'^user/master-data$', views.master_data_upload),
+    url(r'^user/migrate-user-data$', views.migrate_user_data),
+    url(r'^user/failed-records$', views.download_failed_records),
+)
