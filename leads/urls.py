8e7e89e7033469f67443faef409efb95cb813f6e
Basava <basavaraju@regalix-inc.com>
2015-04-07 13:17:18 +0530

    Report issues fixed

---
+++ leads/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import url, patterns
+import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^lead-form/$', views.lead_form),
+    url(r'^bundle-lead-form/$', views.bundle_lead_form),
+    url(r'^leads-list/$', views.leads_list),
+    url(r'^leads-report/$', views.leads_report),
+    url(r'^thankyou/$', views.thankyou),
+    url(r'^lead_error/$', views.lead_error),
+    url(r'^day-light-changes/$', views.day_light_changes),
+    url(r'^manage-leads/$', views.manage_leads),
+    url(r'^upload-leads/$', views.upload_leads),
+    url(r'^migrate-leads/$', views.migrate_leads),
+    url(r'^get-lead/(?P<cid>[\w -]+)$', views.get_lead),
+    url(r'^lead-summary/$', views.get_lead_summary),
+    url(r'^lead-summary/(?P<lid>[0-9]+)$', views.get_lead_summary),
+    url(r'^create-chat/$', views.create_chat_message),
+    url(r'^create-teams/$', views.create_teams),
+    url(r'^create-locations/$', views.create_locations),
+    url(r'^create-codetypes/$', views.create_codetypes),
+    url(r'^agency-form/$', views.agency_form),
+    url(r'^download-agency-csv/$', views.download_agency_csv),
+    url(r'^get-chat/$', views.get_chat_message_by_lead),
+    url(r'^lead-summary-by-user/$', views.get_lead_status_by_ldap),
+    url(r'^agency/(?P<agency_name>[\w -]+)/(?P<pid>[\w -][^/]+)$', views.agent_bulk_upload),
+    url(r'^get-timezones/$', views.get_timezones),
+    url(r'^agency-lead-form/$', views.agency_lead_form),
+    url(r'^leads-summary-by-cid/$', views.get_lead_status_by_cid)
+)
---
+++ reports/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from leads.models import Location, Team
+from datetime import datetime
+
+
+class LeadSummaryReports(models.Model):
+    """ Users additional information """
+    code_type = models.CharField(max_length=100)
+    total_leads = models.IntegerField(max_length=10, default=0)
+    win = models.FloatField(max_length=10, default=0)
+    implemented = models.IntegerField(max_length=10, default=0)
+    in_queue = models.IntegerField(max_length=10, default=0)
+    in_progress = models.IntegerField(max_length=10, default=0)
+    tat_implemented = models.IntegerField(max_length=10, default=0)
+    tat_first_contacted = models.IntegerField(max_length=10, default=0)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+
+class Region(models.Model):
+    """ Region with Location mapping information """
+
+    name = models.CharField(max_length=100, unique=True)
+    location = models.ManyToManyField(Location)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+    def location_list(self):
+        return ", ".join(["%s" % (l.location_name) for l in self.location.all()])
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+    class Meta:
+        db_table = 'regions'
+        ordering = ['name']
+        verbose_name_plural = "Region"
+
+
+class QuarterTargetLeads(models.Model):
+    """ Program by Location mapping in a Quarter
+        Set Target Leads for each Program by Location
+    """
+
+    YEAR_CHOICES = []
+    for r in range(2000, (datetime.utcnow().year + 2)):
+        YEAR_CHOICES.append((r, r))
+
+    program = models.ForeignKey(Team, null=False, blank=False)
+    location = models.ForeignKey(Location, null=False, blank=False)
+    quarter = models.CharField(max_length=10, blank=False, choices=(
+        ('Q1', 'Q1'),
+        ('Q2', 'Q2'),
+        ('Q3', 'Q3'),
+        ('Q4', 'Q4'),)
+    )
+    year = models.IntegerField(max_length=4, choices=YEAR_CHOICES, default=datetime.utcnow().year)
+    target_leads = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('program', 'location', 'quarter', 'year')
+        db_table = 'quarter_target_leads'
+        ordering = ['target_leads']
+        verbose_name_plural = "Quarter Target Leads"
