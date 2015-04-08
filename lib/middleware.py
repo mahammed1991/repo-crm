55f55c42905fc9394f72419c8a2836b9d5bf0937
rajuk_regalix <rajuk_regalix@localhost>
2015-04-08 14:39:49 +0530

    created group called WPP

--- lib/middleware.py
+++ lib/middleware.py
@@ -12,39 +12,44 @@ class SetProfilePicture(object):
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         # get profile picture from google
-        if request.user.is_authenticated() and 'profile_image' not in request.session:
-            try:
-                user_profile = request.user.social_auth.get(uid=request.user.email)
-                user_access_token = user_profile.extra_data['access_token']
+        if request.user.is_authenticated():
+            request.session['groups'] = list()
+            if request.user.groups.filter(name='WPP'):
+                request.session['groups'].append('WPP')
 
-                user_profile_from_google = request_call(
-                    'GET',
-                    'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + user_access_token
-                )
-
-                if user_profile_from_google.status_code == 200:
-                    response = loads(user_profile_from_google.text)
-                    image_url = response['picture']
-                else:
-                    image_url = '/static/images/avtar-big.jpg'
-                request.session['profile_image'] = image_url
+            if 'profile_image' not in request.session:
                 try:
-                    user_profile = UserDetails.objects.get(user_id=request.user.id)
+                    user_profile = request.user.social_auth.get(uid=request.user.email)
+                    user_access_token = user_profile.extra_data['access_token']
+
+                    user_profile_from_google = request_call(
+                        'GET',
+                        'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + user_access_token
+                    )
+
+                    if user_profile_from_google.status_code == 200:
+                        response = loads(user_profile_from_google.text)
+                        image_url = response['picture']
+                    else:
+                        image_url = '/static/images/avtar-big.jpg'
+                    request.session['profile_image'] = image_url
+                    try:
+                        user_profile = UserDetails.objects.get(user_id=request.user.id)
+                    except ObjectDoesNotExist:
+                        user_profile = UserDetails()
+
+                    user_profile.user_id = request.user.id
+                    user_profile.profile_photo_url = image_url
+                    user_profile.save()
+                    request.profile_image_url = image_url
+                    request.session['profile'] = user_profile
+
+                    # List all Locations/Country
+                    locations = Location.objects.exclude(flag_image__isnull=True).filter()
+                    request.session['locations'] = locations
+
                 except ObjectDoesNotExist:
-                    user_profile = UserDetails()
-
-                user_profile.user_id = request.user.id
-                user_profile.profile_photo_url = image_url
-                user_profile.save()
-                request.profile_image_url = image_url
-                request.session['profile'] = user_profile
-
-                # List all Locations/Country
-                locations = Location.objects.exclude(flag_image__isnull=True).filter()
-                request.session['locations'] = locations
-
-            except ObjectDoesNotExist:
-                pass
+                    pass
 
         response = view_func(request, *view_args, **view_kwargs)
         return response
