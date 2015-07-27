c8f102144633fd68e65f688ccf34a43452fc6d23
BasavarajuUS <basavaraju@regalix-inc.com>
2015-07-27 14:51:21 +0530

    Log info for wpp leads data migration

--- timezones_by_channel.py
+++ timezones_by_channel.py
@@ -22,7 +22,11 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "google_portal.settings")
 from leads.models import Leads, WPPLeads
 
+total_leads_before_delete = Leads.objects.all().count()
+print 'Total Number of Leads in Leads is before WPP Leads Delete is', total_leads_before_delete
+
 wpp_leads = Leads.objects.filter(type_1='WPP').values()
+print 'Number of WPP Leads in Leads is ', len(wpp_leads)
 
 for lead in wpp_leads:
     del lead['id']
@@ -32,3 +36,6 @@ for lead in wpp_leads:
     wpp_lead.save()
 
     Leads.objects.filter(sf_lead_id=lead['sf_lead_id']).delete()
+
+total_leads_after_delete = Leads.objects.all().count()
+print 'Total Number of Leads in Leads is before WPP Leads Delete is', total_leads_after_delete
