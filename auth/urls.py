303c20e91b5c367bceeb1a40649355d34d16c9f9
BasavarajuUS <basavaraju@regalix-inc.com>
2015-08-06 17:22:43 +0530

    Lead History Page Implemented

---
+++ auth/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url, include
+import views
+
+urlpatterns = patterns(
+    '',
+    url(r'', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^login$', views.user_login),
+    url(r'^logout$', views.user_logout),
+    url(r'^error$', views.auth_error),
+    url(r'^post_login/$', 'auth.views.post_login'),
+)
