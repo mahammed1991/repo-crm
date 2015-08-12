3ccb7faa69067f89246eabdc4df52908839537b2
rajuk_regalix <rajuk_regalix@localhost>
2015-08-12 21:33:14 +0530

    changed Analytics code to Analytics account ID

---
+++ representatives/migrations/0003_auto__add_field_regalixrepresentatives_email.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'RegalixRepresentatives.email'
+        db.add_column('regalix_representatives', 'email',
+                      self.gf('django.db.models.fields.CharField')(max_length=100, null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'RegalixRepresentatives.email'
+        db.delete_column('regalix_representatives', 'email')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'leads.location': {
+            'Meta': {'ordering': "['location_name']", 'object_name': 'Location', 'db_table': "'locations'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'time_zone': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['leads.Timezone']", 'symmetrical': 'False'})
+        },
+        u'leads.regalixteams': {
+            'Meta': {'ordering': "['team_name']", 'object_name': 'RegalixTeams', 'db_table': "'regalix_teams'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['leads.Location']", 'symmetrical': 'False'}),
+            'team_name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'leads.timezone': {
+            'Meta': {'ordering': "['zone_name']", 'object_name': 'Timezone', 'db_table': "'timezone'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'time_value': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
+            'zone_name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'representatives.availability': {
+            'Meta': {'object_name': 'Availability', 'db_table': "'availability'"},
+            'availability_count': ('django.db.models.fields.IntegerField', [], {}),
+            'booked_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_in_utc': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': u"orm['leads.RegalixTeams']"})
+        },
+        u'representatives.googerepresentatives': {
+            'Meta': {'object_name': 'GoogeRepresentatives', 'db_table': "'google_representatives'"},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 7, 24, 0, 0)'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'role': ('django.db.models.fields.IntegerField', [], {'default': '3'}),
+            'supervisor': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'team': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'user_supporting_region': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'representatives.regalixrepresentatives': {
+            'Meta': {'ordering': "['name']", 'object_name': 'RegalixRepresentatives', 'db_table': "'regalix_representatives'"},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 7, 24, 0, 0)'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'role': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'supervisor': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'team': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'representatives.schedulelog': {
+            'Meta': {'object_name': 'ScheduleLog', 'db_table': "'schedule_log'"},
+            'availability': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['representatives.Availability']"}),
+            'availability_count': ('django.db.models.fields.IntegerField', [], {}),
+            'booked_count': ('django.db.models.fields.IntegerField', [], {}),
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['representatives']
