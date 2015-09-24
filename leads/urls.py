189352cf32d660f830b22dc9a4995ad4120d6238
rajuk_regalix <rajuk_regalix@localhost>
2015-09-24 20:48:18 +0530

    Contact WPP Team with mail trigger

--- leads/urls.py
+++ leads/urls.py
@@ -37,7 +37,8 @@ urlpatterns = patterns(
     url(r'^get-wpp-lead-summary-by-treatment/$', views.get_wpp_lead_summary_by_treatment),
     url(r'^lead-history/(?P<lid>[\w]+)$', views.lead_history),
     url(r'^priority-check/$', views.priority_check),
-    url(r'^check-url-priority/$', views.check_url_priority)
+    url(r'^check-url-priority/$', views.check_url_priority),
+    url(r'^report-team/$', views.report_team)
 
 
 )
