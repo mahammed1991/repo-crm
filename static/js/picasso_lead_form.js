7c3db0d408dbea9ec9071beddc11fdab7ec1a300
Vinay <kvinay@regalix-inc.com>
2015-10-20 17:36:00 +0530

    Removed olark and notification from picasso

---
+++ static/js/picasso_lead_form.js
@@ -0,0 +1,87 @@
+function validatethis(frm) {
+    $(".error-txt").remove();
+    $(".lead-form .form-control").removeClass('error-box');
+    $('.shopping-policy').removeClass('error-box');
+    $('.web-access').removeClass('error-box');
+    // var check = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var check = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var cidFormat = /^\d{3}-\d{3}-\d{4}$/;
+    var phoneFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    var numericExpression = /^[0-9]+$/;
+    window.failedFields = new Array();
+    var fix_slots = new Array();
+    
+    window.is_error = false;
+
+    if(window.is_reset == true){
+      window.is_reset = false;
+      return false;
+    }
+
+    t_typeElem = document.getElementById('picasso_objective')
+    validateFiled(t_typeElem)
+    
+    // Google Rep Name Validation
+    grefElem = document.getElementById('gref');
+    validateFiled(grefElem);
+
+    cidElem = document.getElementById('cid');
+    validateFiled(cidElem);
+
+    if(!$(cidElem).val().match(cidFormat)){
+      $(cidElem).addClass('error-box');
+      /*frm.cid.focus();*/
+      window.is_error = true;
+    }
+
+    companyElem  = document.getElementById('last_name')
+    validateFiled(companyElem)
+
+    urlElem = document.getElementById('url');
+    validateFiled(urlElem);
+
+    countryElem = document.getElementById('code1');
+    validateFiled(countryElem);
+
+    // Check If Error in Form
+    if(window.is_error){
+      focusElem = failedFields[0];
+      $(focusElem).focus();
+      return false;
+    }else{
+      $('#company').val($('#last_name').val());
+      $('#preloaderOverlay').show();
+      return true;
+    }  
+  }
+
+
+  function validateFiled(elem){
+    // Validate Form Field
+    if ($(elem).val() == "" || $(elem).val() == "0" || !$(elem).val()) {
+    $(elem).addClass('error-box');
+    window.failedFields.push(elem);
+    window.is_error = true;
+    return false;
+  }
+}
+
+function validateEmailField(elem) {
+  var check = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // Validate Email Field
+  if (!$(elem).val().trim().match(check)) {
+      $(elem).addClass('error-box');
+      /*$(elem).focus();*/
+      window.is_error = true;
+      return false;
+    }
+}
+
+function resetBtn(elem){
+  elemId = $(elem).attr('id');
+  if(elemId == 'formReset'){
+    window.is_reset = true;
+    window.location.reload();
+  }
+}
+
