c11deaa0d6cf615fb1eaaa736d1136dde18c35b8
BasavarajuUS <basavaraju@regalix-inc.com>
2015-11-19 10:36:07 +0530

    minor fixes

---
+++ reports/migrations/0007_auto__del_csatreport.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'CSATReport'
+        db.delete_table('reports_csatreport')
+
+
+    def backwards(self, orm):
+        # Adding model 'CSATReport'
+        db.create_table('reports_csatreport', (
+            ('category', self.gf('django.db.models.fields.CharField')(default='UNMAPPPED', max_length=10)),
+            ('modified_date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, auto_now_add=True, blank=True)),
+            ('q3', self.gf('django.db.models.fields.IntegerField')(default=0, max_length=10)),
+            ('q2', self.gf('django.db.models.fields.IntegerField')(default=0, max_length=10)),
+            ('q5', self.gf('django.db.models.fields.IntegerField')(default=0, max_length=10)),
+            ('cli', self.gf('django.db.models.fields.CharField')(max_length=100, unique=True)),
+            ('language', self.gf('django.db.models.fields.CharField')(max_length=100, unique=True)),
+            ('q1', self.gf('django.db.models.fields.IntegerField')(default=0, max_length=10)),
+            ('process', self.gf('django.db.models.fields.CharField')(default='TAG', max_length=10)),
+            ('sf_lead_id', self.gf('django.db.models.fields.CharField')(max_length=50, unique=True)),
+            ('survey_date', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('created_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('customer_id', self.gf('django.db.models.fields.CharField')(max_length=100, unique=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('channel', self.gf('django.db.models.fields.CharField')(max_length=10)),
+            ('q4', self.gf('django.db.models.fields.IntegerField')(default=0, max_length=10)),
+        ))
+        db.send_create_signal(u'reports', ['CSATReport'])
+
+
+    models = {
+        u'leads.language': {
+            'Meta': {'object_name': 'Language', 'db_table': "'languages'"},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'language_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'leads.location': {
+            'Meta': {'ordering': "['location_name']", 'object_name': 'Location', 'db_table': "'locations'"},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'daylight_end': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'daylight_start': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'ds_time_zone': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'daylight_timezone'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['leads.Timezone']"}),
+            'flag_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'language': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['leads.Language']", 'symmetrical': 'False'}),
+            'location_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'primary_language': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'related_name': "'primary_language'", 'to': u"orm['leads.Language']"}),
+            'time_zone': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'standard_timezone'", 'symmetrical': 'False', 'to': u"orm['leads.Timezone']"})
+        },
+        u'leads.team': {
+            'Meta': {'ordering': "['team_name']", 'object_name': 'Team', 'db_table': "'teams'"},
+            'belongs_to': ('django.db.models.fields.CharField', [], {'default': "'TAG'", 'max_length': '10'}),
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'team_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        u'leads.timezone': {
+            'Meta': {'ordering': "['zone_name']", 'object_name': 'Timezone', 'db_table': "'timezone'"},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'time_value': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
+            'zone_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'})
+        },
+        u'reports.calllogaccountmanager': {
+            'Meta': {'object_name': 'CallLogAccountManager', 'db_table': "'call_log_account_manager'"},
+            'alternate_number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'call_status': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'log_time_stamp': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'meeting_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'seller_id': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'seller_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'sheet_row_count': ('django.db.models.fields.IntegerField', [], {'default': '0', 'max_length': '10'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'reports.leadsummaryreports': {
+            'Meta': {'object_name': 'LeadSummaryReports'},
+            'code_type': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'end_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'implemented': ('django.db.models.fields.IntegerField', [], {'default': '0', 'max_length': '10'}),
+            'in_progress': ('django.db.models.fields.IntegerField', [], {'default': '0', 'max_length': '10'}),
+            'in_queue': ('django.db.models.fields.IntegerField', [], {'default': '0', 'max_length': '10'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'start_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'tat_first_contacted': ('django.db.models.fields.IntegerField', [], {'default': '0', 'max_length': '10'}),
+            'tat_implemented': ('django.db.models.fields.IntegerField', [], {'default': '0', 'max_length': '10'}),
+            'total_leads': ('django.db.models.fields.IntegerField', [], {'default': '0', 'max_length': '10'}),
+            'win': ('django.db.models.fields.FloatField', [], {'default': '0', 'max_length': '10'})
+        },
+        u'reports.quartertargetleads': {
+            'Meta': {'ordering': "['target_leads']", 'unique_together': "(('program', 'location', 'quarter', 'year'),)", 'object_name': 'QuarterTargetLeads', 'db_table': "'quarter_target_leads'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['leads.Location']"}),
+            'program': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['leads.Team']"}),
+            'quarter': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'target_leads': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'year': ('django.db.models.fields.IntegerField', [], {'default': '2015', 'max_length': '4'})
+        },
+        u'reports.region': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Region', 'db_table': "'regions'"},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['leads.Location']", 'symmetrical': 'False'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['reports']
---
+++ scripts.py
@@ -0,0 +1,268 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+# from django.core.management import setup_environ
+# from rglxtools import settings
+# setup_environ(settings)
+# from leads.models import Location, Timezone
+
+# locations = Location.objects.filter().order_by('location_name')
+
+# location_list = list()
+
+# for location in locations:
+#     timezones = location.objects.all()
+#     print location.location_name
+#     print timezones
+
+# print "Done"
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "google_portal.settings")
+# from leads.models import Leads, WPPLeads
+
+# total_leads_before_delete = Leads.objects.all().count()
+# print 'Total Number of Leads in Leads is before WPP Leads Delete is', total_leads_before_delete
+
+# wpp_leads = Leads.objects.filter(type_1='WPP').values()
+# print 'Number of WPP Leads in Leads is ', len(wpp_leads)
+
+# for lead in wpp_leads:
+#     del lead['id']
+#     lead['treatment_type'] = lead['wpp_treatment_type']
+#     del lead['wpp_treatment_type']
+#     wpp_lead = WPPLeads(**lead)
+#     wpp_lead.save()
+
+#     Leads.objects.filter(sf_lead_id=lead['sf_lead_id']).delete()
+
+# total_leads_after_delete = Leads.objects.all().count()
+# print 'Total Number of Leads in Leads is After WPP Leads Delete is', total_leads_after_delete
+
+# from django.conf import settings
+# import json
+# import gspread
+# from oauth2client.client import SignedJwtAssertionCredentials
+# from reports.models import CallLogAccountManager
+# from datetime import datetime
+
+# json_file = settings.MEDIA_ROOT + '/gtrack-test-0e3eb2372302.json'
+
+# json_key = json.load(open(json_file))
+# scope = ['https://spreadsheets.google.com/feeds']
+
+# credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+
+# gc = gspread.authorize(credentials)
+
+# sheet_url = settings.SPREADSHEET_URL
+
+# worksheet = gc.open_by_url(sheet_url).sheet1
+
+# print worksheet
+
+# all_records = worksheet.get_all_records()
+
+# objects_list = list()
+
+# for record in all_records[1:]:
+#     if record['Meeting Time']:
+#         log_details = CallLogAccountManager()
+#         log_details.username = record['Username']
+#         log_details.seller_name = record['Seller Name']
+#         log_details.seller_id = record['Seller ID']
+#         log_details.phone_number = record['Phone Number']
+#         log_details.alternate_number = record['Alternate Number']
+#         try:
+#             meeting_time_in_cst = datetime.strptime(record['Meeting Time'], "%m/%d/%Y %H:%M:%S")
+#         except Exception:
+#             continue
+#         # Meeting time from cst to ist
+#         # cst_time = datetime.strptime(record[6], "%m/%d/%Y %H:%M:%S")
+#         # tz_cst = Timezone.objects.get(zone_name='CST')
+#         # utc_date = SalesforceApi.get_utc_date(cst_time, tz_cst.time_value)
+#         # tz_ist = Timezone.objects.get(zone_name='IST')
+#         # meeting_time_ist = SalesforceApi.convert_utc_to_timezone(utc_date, tz_ist.time_value)
+
+#         log_details.meeting_time = meeting_time_in_cst  # record['Meeting Time']
+#         log_details.call_status = record['Call Status']
+#         log_details.log_time_stamp = datetime.strptime(record['Timestamp'], "%m/%d/%Y %H:%M:%S")  # record['Timestamp']
+#         log_details.sheet_row_count = worksheet.row_count
+#         objects_list.append(log_details)
+
+# # # total records - 1 saved
+# CallLogAccountManager.objects.bulk_create(objects_list)
+
+from reports.report_services import ReportService
+from django.conf import settings
+from datetime import datetime, timedelta
+import logging
+from lib.salesforce import SalesforceApi
+from leads.models import Leads, SfdcUsers, WPPLeads, PicassoLeads
+from django.core.exceptions import ObjectDoesNotExist
+import pytz
+from representatives.models import GoogeRepresentatives, RegalixRepresentatives
+from oauth2client.client import SignedJwtAssertionCredentials
+from reports.models import CallLogAccountManager
+from datetime import datetime
+
+
+def create_or_update_picasso_leads(records, sf):
+    """ Create a new leads or update existing lead for picasso"""
+    logging.info("Start saving leads to our DB")
+    total_leads = 0
+    new_lead_saved = 0
+    new_lead_failed = 0
+    exist_lead_saved = 0
+    exist_lead_failed = 0
+    is_new_lead = True
+    owners_list = {u.user_id: {'name': u.full_name, 'email': u.email} for u in SfdcUsers.objects.all()}
+    for rec in records:
+        total_leads += 1
+        sf_lead_id = rec.get('Id')
+        type_1 = rec.get('Code_Type__c')
+        try:
+            # check for existing lead
+            lead = PicassoLeads.objects.get(sf_lead_id=sf_lead_id)
+            is_new_lead = False
+        except ObjectDoesNotExist:
+            # create new lead
+            is_new_lead = True
+            lead = PicassoLeads()
+        lead.lead_status = rec.get('Picasso_Lead_Stage__c')
+        lead.type_1 = type_1
+
+        # Google Representative email and name
+        rep_email = rec.get('Email')
+        rep_name = rec.get('Google_Rep__c')
+
+        # Lead owner name
+        owner_id = rec.get('OwnerId')
+        if owner_id and owner_id in owners_list:
+            details = owners_list.get(owner_id)
+            lead_owner_name = details.get('name')
+            lead_owner_email = details.get('email')
+        else:
+            try:
+                user_details = sf.User.get(owner_id)
+                lead_owner_name = user_details.get('Name')
+                lead_owner_email = user_details.get('Email')
+            except ObjectDoesNotExist:
+                lead_owner_name = "%s %s" % (settings.DEFAULT_LEAD_OWNER_FNAME, settings.DEFAULT_LEAD_OWNER_LNAME)
+                lead_owner_email = settings.DEFAULT_LEAD_OWNER_EMAIL
+
+        # Team
+        team = rec.get('Team__c') if rec.get('Team__c') else ''
+
+        # Below information will be created if its a new lead or else the information will be updated
+        lead.google_rep_name = rep_name
+        lead.google_rep_email = rep_email if rep_email else settings.DEFAULT_LEAD_OWNER_EMAIL
+
+        if rep_email and rep_name:
+            # Save Google representatives information to Database
+            try:
+                GoogeRepresentatives.objects.get(email=rep_email)
+            except ObjectDoesNotExist:
+                google_rep = GoogeRepresentatives()
+                rep_name = rep_name.split(' ')
+                google_rep.first_name = unicode(rep_name[0])
+                google_rep.last_name = unicode((' ').join(rep_name[1:]))
+                google_rep.email = unicode(rep_email)
+                google_rep.team = team
+                google_rep.save()
+
+        # Save Regalix representatives information to Database
+        if lead_owner_email and lead_owner_name:
+            try:
+                RegalixRepresentatives.objects.get(email=lead_owner_email)
+            except ObjectDoesNotExist:
+                regalix_rep = RegalixRepresentatives()
+                regalix_rep.name = lead_owner_name
+                regalix_rep.email = lead_owner_email
+                regalix_rep.team = team
+                regalix_rep.save()
+
+        # check if column is formatted to date type
+        # if it is of date type, convert to datetime object
+        created_date = rec.get('CreatedDate')
+        created_date = SalesforceApi.salesforce_date_to_datetime_format(created_date)
+        if not created_date:
+            created_date = datetime.utcnow()
+
+        lead.created_date = created_date
+
+        lead.lead_owner_name = lead_owner_name if lead_owner_name else "%s %s" % (settings.DEFAULT_LEAD_OWNER_FNAME,
+                                                                                  settings.DEFAULT_LEAD_OWNER_LNAME)
+        lead.lead_owner_email = lead_owner_email if lead_owner_email else settings.DEFAULT_LEAD_OWNER_EMAIL
+        lead.company = unicode(rec.get('Company'))
+        lead.country = rec.get('Location__c')
+
+        cid = rec.get('Customer_ID__c')
+        internal_cid = rec.get('Internal_CID_1__c')  # for live we have to change
+        if type(cid) is float:
+            lead.customer_id = int(cid)
+        else:
+            lead.customer_id = cid
+
+        lead.internal_cid = internal_cid
+        lead.first_name = unicode(rec.get('FirstName'))
+        lead.last_name = unicode(rec.get('LastName'))
+        lead.phone = unicode(rec.get('Phone'))
+
+        # check if column is formatted to date type
+        # if it is of date type, convert to datetime object
+        date_of_installation = rec.get('Date_of_installation__c')
+        date_of_installation = SalesforceApi.salesforce_date_to_datetime_format(date_of_installation)
+        lead.date_of_installation = date_of_installation
+
+        lead.regalix_comment = unicode(rec.get('All_Regalix_Comments__c')).encode('unicode_escape')
+        lead.google_comment = unicode(rec.get('Google_Comment__c')).encode('unicode_escape')
+
+        lead.code_1 = rec.get('Code__c') if rec.get('Code__c') else ''
+        lead.url_1 = rec.get('URL__c') if rec.get('URL__c') else ''
+        lead.type_1 = rec.get('Code_Type__c') if rec.get('Code_Type__c') else ''
+        lead.comment_1 = rec.get('Comment_1__c') if rec.get('Comment_1__c') else ''
+
+        lead.team = team
+        lead.sf_lead_id = sf_lead_id
+        lead.picasso_objective = (rec.get('Picasso_Objective__c')).replace(';', ',') if rec.get('Picasso_Objective__c') else ''
+        lead.pod_name = rec.get('POD_Name__c') if rec.get('POD_Name__c') else ''
+
+        try:
+            lead.save()
+            if is_new_lead:
+                new_lead_saved += 1
+            else:
+                exist_lead_saved += 1
+        except Exception as e:
+            print lead.sf_lead_id, e
+            if is_new_lead:
+                new_lead_failed += 1
+            else:
+                exist_lead_failed += 1
+
+    logging.info("**********************************************************")
+    logging.info("Total Picasso leads saved to our DB: %s" % (total_leads))
+    logging.info("New Picasso Leads count: %s" % (new_lead_saved))
+    logging.info("New Picasso Leads Failed Count: %s" % (new_lead_failed))
+    logging.info("Exist Picasso leads updated Count: %s" % (exist_lead_saved))
+    logging.info("Exist Picasso lead failed to update: %s" % (exist_lead_failed))
+
+end_date = datetime.now(pytz.UTC)    # we need to use UTC as salesforce API requires this
+start_date = end_date - timedelta(days=30)
+start_date = SalesforceApi.convert_date_to_salesforce_format(start_date)
+end_date = SalesforceApi.convert_date_to_salesforce_format(end_date)
+sf = SalesforceApi.connect_salesforce()
+select_items = settings.SFDC_FIELDS
+tech_team_id = settings.TECH_TEAM_ID
+code_type = 'WPP'
+where_clause_picasso = "WHERE (LastModifiedDate >= %s AND LastModifiedDate <= %s)" % (start_date, end_date)
+sql_query_picasso = "select %s from Lead %s" % (select_items, where_clause_picasso)
+try:
+    picasso_leads = sf.query_all(sql_query_picasso)
+    create_or_update_picasso_leads(picasso_leads['records'], sf)
+except Exception as e:
+    print e
+    logging.info("Fail to get updated leads from %s to %s" % (start_date, end_date))
+    logging.info("%s" % (e))
