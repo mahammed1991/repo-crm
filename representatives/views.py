045a10d538bc7328722e531b795f85402fd4315a
Vinay <kvinay@regalix-inc.com>
2015-12-04 11:29:56 +0530

    Export Appointments: Removed old version and updated latest and Total Appointments: Removed extra code in html

--- representatives/views.py
+++ representatives/views.py
@@ -693,7 +693,7 @@ def copy_appointment_to_next_week(request, plan_month=0, plan_day=0, plan_year=0
     return HttpResponse(json.dumps(response), content_type="application/json")
 
 @login_required
-def export_appointments_new(request):
+def export_appointments(request):
     """ Export Appointments by Availability/Booked """
 
     default_process_type = ['TAG', 'SHOPPING', 'WPP']
@@ -830,137 +830,11 @@ def export_appointments_new(request):
         response = DownloadLeads.get_downloaded_file_response(path)
         return response
 
-    return render(request, 'representatives/export_appointments_new.html', {'teams': teams,
+    return render(request, 'representatives/export_appointments.html', {'teams': teams,
                                                                         'default_teams': default_teams,
                                                                         'process_types': process_types,
                                                                         'default_process_type': default_process_type,
                                                                         'post_result_dict': json.dumps(post_result_dict),
-                                                                        'tag_by_team': tag_by_team})
-
-
-@login_required
-def export_appointments(request):
-    """ Export Appointments by Availability/Booked """
-
-    process_types = RegalixTeams.objects.exclude(process_type='MIGRATION').values_list('process_type', flat=True).distinct().order_by()
-    teams = RegalixTeams.objects.filter(process_type__in=process_types).exclude(team_name='default team')
-
-    tag_by_team = dict()
-    for team in teams:
-        rec = {
-            'name': str(team.team_name),
-            'id': str(team.id)
-        }
-        if team.process_type not in tag_by_team:
-            tag_by_team[str(team.process_type)] = [rec]
-        else:
-            tag_by_team[str(team.process_type)].append(rec)
-
-    if request.method == 'POST':
-        from_date = request.POST.get('date_from')
-        to_date = request.POST.get('date_to')
-        process_type = request.POST.get('process_type')
-        regalix_team = request.POST.get('team')
-        # appointment_type = request.POST.get('appointment-type')
-
-        from_date = datetime.strptime(from_date, "%b %d, %Y")
-        to_date = datetime.strptime(to_date, "%b %d, %Y")
-        to_date = datetime(to_date.year, to_date.month, to_date.day, 23, 59, 59)
-
-        time_zone = 'IST'
-        selected_tzone = Timezone.objects.get(zone_name=time_zone)
-        from_utc_date = SalesforceApi.get_utc_date(from_date, selected_tzone.time_value)
-        to_utc_date = SalesforceApi.get_utc_date(to_date, selected_tzone.time_value)
-        diff = divmod((from_utc_date - from_date).total_seconds(), 60)
-        diff_in_minutes = diff[0]
-
-        if regalix_team == 'all':
-            regalix_teams = RegalixTeams.objects.filter(process_type=process_type).exclude(team_name='default team')
-        else:
-            regalix_teams = RegalixTeams.objects.filter(process_type=process_type, id=regalix_team)
-
-        collumn_attr = ['Hours', 'Team']
-        s_date = from_date
-        while True:
-            if s_date <= to_date:
-                collumn_attr.append(datetime.strftime(s_date, "%d/%b/%Y"))
-                s_date = s_date + timedelta(days=1)
-            else:
-                break
-
-        total_result = list()
-        for rglx_team in regalix_teams:
-            # get all appointments for selected dates in given range
-            slots_data = Availability.objects.filter(
-                date_in_utc__range=(from_utc_date, to_utc_date),
-                team__id=rglx_team.id,
-                team__process_type=process_type
-            ).order_by('team')
-
-            result = list()
-            team_name = rglx_team.team_name
-
-            for i in range(0, 24):
-                for j in ['00', '30']:
-                    mydict = {}
-                    if len(str(i)) == 1:
-                        indx = '0%s' % (i)
-                    else:
-                        indx = str(i)
-                    hour = "%s:%s" % (indx, j)
-                    for ele in collumn_attr:
-                        if ele == 'Team':
-                            mydict[ele] = team_name
-                        elif ele == 'Hours':
-                            mydict[ele] = hour
-                        else:
-                            mydict[ele] = '0|0'
-
-                    result.append(mydict)
-
-            total_appointments = dict()
-            for slot in slots_data:
-                # time zone conversion
-                requested_date = slot.date_in_utc
-                requested_date -= timedelta(minutes=diff_in_minutes)
-                _date = datetime.strftime(requested_date, "%d/%b/%Y")
-                _time = datetime.strftime(requested_date, "%H:%M")
-                availability_count = slot.availability_count
-                booked_count = slot.booked_count
-                if _date in total_appointments:
-                    total_appointments[_date]['booked_count'] += booked_count
-                    total_appointments[_date]['availability_count'] += availability_count
-                else:
-                    total_appointments[_date] = {'booked_count': booked_count, 'availability_count': availability_count}
-                val = "%s|%s" % (booked_count, availability_count)
-
-                for rec in result:
-                    if str(_time) in rec.values():
-                        rec[_date] = val
-
-            total_dict = dict()
-            for ele in collumn_attr:
-                if ele == 'Team':
-                    total_dict[ele] = ''
-                elif ele == 'Hours':
-                    total_dict[ele] = 'Total'
-                else:
-                    total_dict[ele] = '0|0'
-
-            for _date_ele in total_appointments:
-                if _date_ele in total_dict:
-                    total_dict[_date_ele] = "%s|%s" % (str(total_appointments[_date_ele]['booked_count']), str(total_appointments[_date_ele]['availability_count']))
-
-            total_result.extend(result)
-            total_result.append(total_dict)
-
-        filename = "appointments-%s-to-%s" % (datetime.strftime(from_date, "%b-%d-%Y"), datetime.strftime(to_date, "%b-%d-%Y"))
-        path = write_appointments_to_csv(total_result, collumn_attr, filename)
-        response = DownloadLeads.get_downloaded_file_response(path)
-        return response
-
-    return render(request, 'representatives/export_appointments.html', {'teams': teams,
-                                                                        'process_types': process_types,
                                                                         'tag_by_team': tag_by_team})
 
 
