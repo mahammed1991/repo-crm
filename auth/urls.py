9ce782357ecbebde5cc99703d93687229f885ef7
mahammed <mahammed@localhost>
2016-02-03 18:57:14 +0530

    kick-off-form-validation-added

---
+++ auth/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url, include
+import views
+
+urlpatterns = patterns(
+    '',
+    url(r'', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^login$', views.user_login),
+    url(r'^logout$', views.user_logout),
+    url(r'^error$', views.auth_error),
+    url(r'^post_login/$', 'auth.views.post_login'),
+    url(r'^redirect_domain/$', 'auth.views.redirect_domain'),
+    url(r'^current_domain/$', 'auth.views.current_domain'),
+)
