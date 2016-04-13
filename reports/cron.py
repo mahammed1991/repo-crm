da71af3b814449d684f1df043df0fa64e0a2434f
mahammed <mahammed@localhost>
2016-04-13 12:10:22 +0530

    removed local word from subject line

--- reports/cron.py
+++ reports/cron.py
@@ -978,7 +978,7 @@ def slots_open_booked():
     specific_date_time = datetime.today()
     specific_time = datetime.strftime(specific_date_time, '%H:%M:%S')
     logging.info("UTILIZATION DASHBOARD MAILING FUNCTION")
-    mail_subject = "Local[TAG & SHOPPING] SLOT UTILIZATION DASHBOARD-%s %s" % (specific_date, specific_time)
+    mail_subject = "[TAG & SHOPPING] SLOT UTILIZATION DASHBOARD-%s %s" % (specific_date, specific_time)
     mail_body = get_template('reports/email_templates/slots_detail.html').render(Context({'tag':tag_final,'shopp':shopping_final,'tag_total_sum_sorted':tag_total_sum_sorted, 'shopping_total_sum_sorted':shopping_total_sum_sorted, 'mail_trigerring_date':specific_date }))
     mail_from = 'google@regalix-inc.com'
     mail_to = ['portalsupport@regalix-inc.com']
