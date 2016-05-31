9e0d63f2db180fa8fe203e687902add6cba1c16b
Arun007 <Arun007@localhost>
2016-05-31 20:47:10 +0530

    small changes in filehandling func

--- google_portal/urls.py
+++ google_portal/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns(
