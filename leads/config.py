ad15d15647d35dd65f9e1446d516ba0f36e072b4
Anubolu Sreekanth <anubolu.sreekanth@regalix-inc.com>
2016-06-17 12:14:03 +0530

    RLSA Bulk Upload Fixes and cron job change to update build eligible changes

---
+++ leads/config.py
@@ -0,0 +1,10 @@
+
+""" RLSA Bulk upload coniguration  """
+RLSA_BULK_CSV_HEADERS = ['Advertiser Name', 'Advertiser Company', 'Advertiser Email', 'Advertiser Phone',
+ 'Advertiser Location', 'Advertiser Language', 'Auth E-mail gCases Link ', 'Apply to',
+ 'Comment', 'Advertiser Customer ID', 'User List ID 1', 'RLSA Bid Adjustment 1',
+ 'User List ID 2', 'RLSA Bid Adjustment 2', 'User List ID 3', 'RLSA Bid Adjustment 3',
+ 'User List ID 4', 'RLSA Bid Adjustment 4', 'User List ID 5', 'RLSA Bid Adjustment 5']
+
+
+
