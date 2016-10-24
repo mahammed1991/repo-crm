1cd6d9f23dd2959549c242218687c23f0feeefaf
mahammed <mahammed@localhost>
2016-10-24 19:44:31 +0530

    search functionality based on cid and sf lead id, template added and no leads image added

---
+++ mini_crm/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, patterns
+import views
+from django.views.generic import RedirectView, TemplateView
+
+urlpatterns = patterns(
+    '',
+    url(r'^all-leads/$', views.crm_management),
+    url(r'^myleads/$', views.crm_agent),
+    url(r'^search-leads/', views.search_leads),
+)
