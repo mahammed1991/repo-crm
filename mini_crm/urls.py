c9b0eec588bbc27179f9e0aee3f9a0389a41e4c4
Arun007 <Arun007@localhost>
2016-10-25 19:07:13 +0530

    individual lead view functionality

--- mini_crm/urls.py
+++ mini_crm/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns(
     '',
     url(r'^all-leads/$', views.crm_management),
     url(r'^myleads/$', views.crm_agent),
+    url(r'^lead-details/(?P<lid>[0-9]+)/(?P<sf_lead_id>\w+)$',views.lead_details),
+    url(r'^lead-owner-email',views.lead_owner_avalibility),
     url(r'^lead-history/$',views.lead_history),
     url(r'^search-leads/', views.search_leads),
 )
