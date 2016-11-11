2a8001a9c8217a7155fa12a0d47544185a76d822
sheetalsharma <sheetalsharma@localhost>
2016-11-11 15:25:00 +0530

    merge

--- mini_crm/urls.py
+++ mini_crm/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns(
     url(r'^lead-details/(?P<lid>[0-9]+)/(?P<sf_lead_id>\w+)/(?P<ctype>\w+)$',views.lead_details),
     url(r'^lead-owner-email',views.lead_owner_avalibility),
     url(r'^update-lead/$',views.update_lead),
+    url(r'^add-lead-comment/$',views.add_lead_comment),
     url(r'^lead-history/$',views.lead_history),
     url(r'^search-leads/', views.search_leads),
     url(r'^crm-agents/emails/$',views.get_crm_agents_emails),
@@ -16,5 +17,6 @@ urlpatterns = patterns(
     url(r'^delete-lead/(?P<lid>[0-9]+)/(?P<ctype>\w+)$', views.delete_lead, name="delete-lead"),
     url(r'^get-lead-history/$',views.get_lead_history),
     url(r'^save-image-file/$',views.save_image_file),
-    url(r'^download-image-file/',views.download_image_file)
+    url(r'^download-image-file/',views.download_image_file),
+    url(r'^get-lead-sub-status/$', views.get_lead_sub_status),
 )
