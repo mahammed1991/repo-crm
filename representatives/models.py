60af649f21f96985b2987d9d2318038da883a57f
Anubolu Sreekanth <anubolu.sreekanth@regalix-inc.com>
2016-11-22 20:52:44 +0530

    Minor fixes

---
+++ representatives/models.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.conf import settings
+
+from leads.models import RegalixTeams
+
+
+# Create your models here.
+class GoogeRepresentatives(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+    role = models.IntegerField(default=settings.GOOGLE_DEFAULT_ROLE, null=False)
+    user_supporting_region = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    supervisor = models.CharField(max_length=100)
+
+    created_date = models.DateTimeField(default=datetime.utcnow())
+
+    class Meta:
+        db_table = 'google_representatives'
+
+
+class RegalixRepresentatives(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, null=True)
+    supervisor = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    role = models.IntegerField(default=settings.REGALIX_DEFAULT_ROLE, null=False)
+
+    created_date = models.DateTimeField(default=datetime.utcnow())
+
+    class Meta:
+        db_table = 'regalix_representatives'
+        ordering = ['name']
+
+
+class Availability(models.Model):
+    availability_count = models.IntegerField()
+    booked_count = models.IntegerField(default=0)
+    date_in_utc = models.DateTimeField()
+    team = models.ForeignKey(RegalixTeams, default=1)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+    class Meta:
+        db_table = 'availability'
+
+
+class ScheduleLog(models.Model):
+    user = models.ForeignKey(User)
+    availability = models.ForeignKey(Availability)
+    availability_count = models.IntegerField()
+    booked_count = models.IntegerField()
+    description = models.TextField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'schedule_log'
+        verbose_name_plural = "Schedule Log"
+
+
+class AvailabilityForTAT(models.Model):
+    date_in_ist = models.DateTimeField()
+    availability_count = models.IntegerField(default=0)
+    audits_per_date = models.IntegerField(default=0)
+    process = models.CharField(max_length=100)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+    class Meta:
+        db_table = 'availability_for_tat'
+
+
+class AvailabilityForBoltTAT(models.Model):
+    date_in_ist = models.DateTimeField()
+    availability_count = models.IntegerField(default=0)
+    audits_per_date = models.IntegerField(default=0)
+    process = models.CharField(max_length=100)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+    class Meta:
+        db_table = 'availability_for_bolt_tat'
